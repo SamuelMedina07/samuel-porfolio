df4ffcc7b7e0f0c4b8ead3ebf223b22f32fdc81f
SamuelMedina07 <semedina@uth.hn>
2025-08-15 22:11:20 -0600

    Hola

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
+
 import robotsTxt from "astro-robots-txt"
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  outDir: 'docs', // bien para GitHub Pages si usas rama main
-  site: 'https://samuelmedina07.github.io',
+  site: 'https://samuelmedina07.github.io/',
   base: '/samuel-porfolio/',
+  outDir:'docs'
 })
+
+
+
+
+
+
 
