9538b233f112ec19edc6a8dba64ba788ba39f389
SamuelMedina07 <semedina@uth.hn>
2025-08-15 19:09:38 -0600

    Add prod branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
+  outDir: 'docs',
   base: '/samuel-porfolio/'
 })
