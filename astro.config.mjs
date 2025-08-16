04247b18dd7df54802e7092c19cea801828918ad
SamuelMedina07 <semedina@uth.hn>
2025-08-16 07:25:14 -0600

    Morning Routine

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://samuelmedina07.github.io/samuel-porfolio/',
   base: '/samuel-porfolio/',
-  outDir: 'docs', // good for GitHub Pages if you're using the main branch
+  outDir: 'docs' // good for GitHub Pages if you're using the main branch
 });
-
-
-
-
-
-
